80fa9bc030dc1a3e3a8d7f4a97844483152b3eec
Valentin <valentintrofin14@gmail.com>
2022-04-13 15:53:14 +0300

    functionalitate buton Connect

--- app/src/main/java/com/example/pip_robot/MainActivity.java
+++ app/src/main/java/com/example/pip_robot/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.pip_robot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.connect);
+        setContentView(R.layout.activity_main);
+        ImageButton search = (ImageButton)findViewById(R.id.imageButton);
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.connect);
+            }
+        });
     }
 }
