4fd2f161c2e19cd873dd68981026d4d620016ffa
Valentin <valentintrofin14@gmail.com>
2022-04-14 19:21:07 +0300

    adaugare controls page si commands page

--- app/src/main/java/com/example/pip_robot/MainActivity.java
+++ app/src/main/java/com/example/pip_robot/MainActivity.java
@@ -3,8 +3,8 @@ package com.example.pip_robot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,12 +13,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageButton search = (ImageButton)findViewById(R.id.imageButton);
-        search.setOnClickListener(new View.OnClickListener() {
+
+        ImageButton connectButton = (ImageButton)findViewById(R.id.connectButton);
+        connectButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                setContentView(R.layout.connect);
-            }
+            public void onClick(View v) { openConnectActivity(); }
         });
+
+        ImageButton controlsButton = (ImageButton)findViewById(R.id.controlsButton);
+        controlsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { openControlsActivity(); }
+        });
+        ImageButton commandsButton = (ImageButton)findViewById(R.id.commandsButton);
+        commandsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { openCommandsActivity(); }
+        });
+    }
+    public void openConnectActivity() {
+        Intent intent = new Intent(this, ConnectActivity.class);
+        startActivity(intent);
+    }
+    public void openControlsActivity() {
+        Intent intent = new Intent(this, ControlsActivity.class);
+        startActivity(intent);
+    }
+    public void openCommandsActivity() {
+        Intent intent = new Intent(this, CommandsActivity.class);
+        startActivity(intent);
     }
 }
