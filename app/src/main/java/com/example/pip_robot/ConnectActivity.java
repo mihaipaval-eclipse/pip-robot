61c5d2732b4bfb13c249f5bd786779044b942572
Valentin <valentintrofin14@gmail.com>
2022-05-04 22:48:38 +0300

    pus la punct

--- app/src/main/java/com/example/pip_robot/ConnectActivity.java
+++ app/src/main/java/com/example/pip_robot/ConnectActivity.java
@@ -2,15 +2,11 @@ package com.example.pip_robot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.net.*;
-import java.io.*;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.Toast;
 
 
 public class ConnectActivity extends AppCompatActivity {
@@ -28,23 +24,20 @@ public class ConnectActivity extends AppCompatActivity {
         connect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.println(Log.INFO,"aaa", GetIp());
-                IP=GetIp();
-                PORT= GetPort();
+                Log.println(Log.INFO,"ip", GetIP());
+                IP = GetIP();
+                PORT = GetPort();
             }
         });
     }
-
-    public String GetIp() {
-        EditText ip = (EditText) findViewById(R.id.ipid);
+    public String GetIP() {
+        EditText ip = (EditText) findViewById(R.id.ipBox);
         String message = ip.getText().toString();
         return message;
     }
-
     public int GetPort() {
-        EditText ip = (EditText) findViewById(R.id.portid);
+        EditText ip = (EditText) findViewById(R.id.portBox);
         String message = ip.getText().toString();
         return Integer.parseInt(message);
     }
-
 }
