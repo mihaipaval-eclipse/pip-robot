00d58a4cdcf58ff1fd1f859a3578c594aee0af65
Valentin <valentintrofin14@gmail.com>
2022-05-05 12:09:54 +0300

    changes

--- app/src/main/java/com/example/pip_robot/ConnectActivity.java
+++ app/src/main/java/com/example/pip_robot/ConnectActivity.java
@@ -18,6 +18,7 @@ public class ConnectActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect);
         getSupportActionBar().hide();
+        // test
 
         Button connect = (Button)findViewById(R.id.connect);
         connect.setOnClickListener(new View.OnClickListener() {
