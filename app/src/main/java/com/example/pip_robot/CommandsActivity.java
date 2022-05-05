a844415c65e248ba3ddf5edde33cfcebd31271e3
Valentin <valentintrofin14@gmail.com>
2022-05-05 14:14:20 +0300

    adaugata consola

--- app/src/main/java/com/example/pip_robot/CommandsActivity.java
+++ app/src/main/java/com/example/pip_robot/CommandsActivity.java
@@ -3,10 +3,11 @@ package com.example.pip_robot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CommandsActivity extends AppCompatActivity {
 
@@ -15,5 +16,23 @@ public class CommandsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_commands);
         getSupportActionBar().hide();
+
+        TextView console = (TextView) findViewById(R.id.console);
+        console.setMovementMethod(new ScrollingMovementMethod());
+
+        Button type = (Button) findViewById(R.id.type);
+        type.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (console.getText().toString().matches(""))
+                    console.append(GetCommand());
+                else
+                    console.append("\n" + GetCommand());
+            }
+        });
+    }
+    public String GetCommand() {
+        EditText commandBox = (EditText) findViewById(R.id.commandBox);
+        return commandBox.getText().toString();
     }
 }
