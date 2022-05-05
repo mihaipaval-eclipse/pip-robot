22b609f94e3506678a2433ce37bedabcb62a2218
Valentin <valentintrofin14@gmail.com>
2022-05-05 13:20:14 +0300

    implementare Client

--- app/src/main/java/com/example/pip_robot/ConnectActivity.java
+++ app/src/main/java/com/example/pip_robot/ConnectActivity.java
@@ -3,14 +3,23 @@ package com.example.pip_robot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class ConnectActivity extends AppCompatActivity {
 
     static String IP = null;
     static int PORT = 0;
+    static Socket socket = null;
+    static PrintWriter out = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,5 +42,35 @@ public class ConnectActivity extends AppCompatActivity {
     public int GetPort() {
         EditText portBox = (EditText) findViewById(R.id.portBox);
         return Integer.parseInt(portBox.getText().toString());
+    }
+    static public void Client(String address, int port, String command){
+        try {
+            socket = new Socket(InetAddress.getByName(address), port);
+            Log.println(Log.INFO,"c_s", "Connected");
+            out = new PrintWriter(socket.getOutputStream(),true);
+        } catch(UnknownHostException e){
+            Log.println(Log.INFO,"c_uhe", "Don't know about host: " + address);
+        } catch (IOException e){
+            Log.println(Log.INFO,"c_e", "Couldn't get I/O for the connection to: " + address);
+        }
+        if (socket == null || out == null) {
+            Log.println(Log.INFO,"c_e_s", "Something is wrong. One variable is null.");
+            return;
+        }
+
+        out.write(command +"\n");
+        out.flush();
+
+        // close the connection
+
+        out.close();
+        try {
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        boolean verificam_inchidere_conexiune = socket.isClosed();
+        Log.println(Log.INFO,"verificare", "Socket closed: "+ verificam_inchidere_conexiune);
     }
 }
--- app/src/main/java/com/example/pip_robot/ControlsActivity.java
+++ app/src/main/java/com/example/pip_robot/ControlsActivity.java
@@ -29,11 +29,7 @@ public class ControlsActivity extends AppCompatActivity {
         Button art1 = (Button)findViewById(R.id.art1);
         art1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                replaceFragment(new art1fragment());
-                Client(ConnectActivity.IP, ConnectActivity.PORT);
-                Log.println(Log.INFO,"test", "IP: " + ConnectActivity.IP + " & PORT: " + ConnectActivity.PORT);
-            }
+            public void onClick(View v) { replaceFragment(new art1fragment()); }
         });
         Button art2 = (Button)findViewById(R.id.art2);
         art2.setOnClickListener(new View.OnClickListener() {
@@ -61,37 +57,5 @@ public class ControlsActivity extends AppCompatActivity {
         FragmentTransaction ft = fg.beginTransaction();
         ft.replace(R.id.flFragment, fragment);
         ft.commit();
-    }
-    public void Client(String address, int port){
-        Socket socket = null;
-        PrintWriter out = null;
-        try {
-            socket = new Socket(InetAddress.getByName(address), port);
-            Log.println(Log.INFO,"c_s", "Connected");
-            out = new PrintWriter(socket.getOutputStream(),true);
-        } catch(UnknownHostException e){
-            Log.println(Log.INFO,"c_uhe", "Don't know about host: " + address);
-        } catch (IOException e){
-            Log.println(Log.INFO,"c_e", "Couldn't get I/O for the connection to: " + address);
-        }
-        if (socket == null || out == null) {
-            Log.println(Log.INFO,"c_e_s", "Something is wrong. One variable is null.");
-            return;
-        }
-
-        out.write("set_digital_out(0,True)"+"\n");
-        out.flush();
-
-        // close the connection
-
-        out.close();
-        try {
-            socket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        boolean verificam_inchidere_conexiune = socket.isClosed();
-        Log.println(Log.INFO,"verificare", "Socket closed: "+ verificam_inchidere_conexiune);
     }
 }
