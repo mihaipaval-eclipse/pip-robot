e88cf25404f9d31cf50209b1b9c2546493fd2598
Valentin <valentintrofin14@gmail.com>
2022-05-05 21:16:05 +0300

    up to date

---
+++ app/src/main/java/com/example/pip_robot/art5fragment.java
@@ -0,0 +1,18 @@
+package com.example.pip_robot;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class art5fragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_art5fragment, container, false);
+    }
+}
